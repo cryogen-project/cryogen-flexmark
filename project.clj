5e58ba420d1fc521c4034aa4c383b81b01b67341
Jane Olszewska <3jane@3jane.co.uk>
2020-03-06 18:05:07 +0000

    Package version bumped: missing footnotes regression fixed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-flexmark "0.1.1"
+(defproject cryogen-flexmark "0.1.2"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-flexmark"
   :license {:name "BSD 2"
