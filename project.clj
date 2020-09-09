61e687037af298c2061aecd2016b4c911df80d0a
Dorab Patel <dorabpatel@gmail.com>
2020-09-09 15:57:26 -0700

    Allow multiple extensions in  Markup.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject cryogen-flexmark "0.1.2"
+(defproject cryogen-flexmark "0.1.3"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-flexmark"
   :license {:name "BSD 2"
             :url "https://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cryogen-core "0.3.1"]
+                 [cryogen-core "0.3.2"]
                  [com.vladsch.flexmark/flexmark "0.60.2"]
                  [com.vladsch.flexmark/flexmark-util "0.60.2"]
                  [com.vladsch.flexmark/flexmark-ext-footnotes "0.60.2"]
