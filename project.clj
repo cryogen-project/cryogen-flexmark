4441aaab58b57238a4343be647041af2802a4fe2
Dorab Patel <dorabpatel@gmail.com>
2020-10-16 19:52:17 -0700

    Bump version to 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cryogen-flexmark "0.1.3"
+(defproject cryogen-flexmark "0.1.4"
   :description "Markdown parser for Cryogen"
   :url "https://github.com/cryogen-project/cryogen-flexmark"
   :license {:name "BSD 2"
